dce2ee066434ea4952d17a57e205baf1512a1b14
puc-void <215346186+puc-void@users.noreply.github.com>
2026-06-11 20:51:48 +0600

    button functionality added with click

--- tracker.js
+++ tracker.js
@@ -4,3 +4,21 @@ const totalJobs = document.querySelectorAll(
 
 document.getElementById("total-count").innerText = totalJobs;
 document.getElementById("job-count-text").innerText = `${totalJobs} jobs`;
+
+
+function showSection(sectionId) {
+
+    const allJobs=document.getElementById("all-jobs-section");
+    const interview=document.getElementById("interview-section");
+    const rejected=document.getElementById("rejected-section");
+
+    allJobs.classList.add("hidden");
+    interview.classList.add("hidden");
+    rejected.classList.add("hidden");
+
+    const selected = document.getElementById(sectionId);
+    selected.classList.remove("hidden");
+
+
+    // Implementation for showing a specific section
+}
