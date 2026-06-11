32c3091ef23d20dba31c61bb72a1fcad31ebcc32
puc-void <215346186+puc-void@users.noreply.github.com>
2026-06-11 20:41:22 +0600

    job counter and label changers js added

---
+++ tracker.js
@@ -0,0 +1,6 @@
+const totalJobs = document.querySelectorAll(
+    "#all-jobs-section .card"
+).length;
+
+document.getElementById("total-count").innerText = totalJobs;
+document.getElementById("job-count-text").innerText = `${totalJobs} jobs`;
