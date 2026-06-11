7c26b11155d353f904e05b444d1d2edc2815c018
puc-void <215346186+puc-void@users.noreply.github.com>
2026-06-11 21:20:32 +0600

    button focused color change by js

--- tracker.js
+++ tracker.js
@@ -8,17 +8,41 @@ document.getElementById("job-count-text").innerText = `${totalJobs} jobs`;
 
 function showSection(sectionId) {
 
-    const allJobs=document.getElementById("all-jobs-section");
-    const interview=document.getElementById("interview-section");
-    const rejected=document.getElementById("rejected-section");
+    document.getElementById("all-jobs-section").classList.add("hidden");
+    document.getElementById("interview-section").classList.add("hidden");
+    document.getElementById("rejected-section").classList.add("hidden");
 
-    allJobs.classList.add("hidden");
-    interview.classList.add("hidden");
-    rejected.classList.add("hidden");
-
-    const selected = document.getElementById(sectionId);
-    selected.classList.remove("hidden");
+    document.getElementById(sectionId).classList.remove("hidden");
+}
 
 
-    // Implementation for showing a specific section
+
+
+//bade change korar jonno
+document.querySelectorAll(".interview-btn").forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        const card = this.closest(".card");
+
+        const badge = card.querySelector(".status-badge");
+
+        badge.innerText = "Interview";
+
+        badge.classList.remove("badge-outline");
+        badge.classList.add("badge-success");
+
+    });
+
+});
+
+
+//button color change korar jonno
+function setActive(clickedBtn) {
+
+    document.querySelectorAll(".nav-btn").forEach(btn => {
+        btn.classList.remove("btn-primary");
+    });
+
+    clickedBtn.classList.add("btn-primary");
 }
